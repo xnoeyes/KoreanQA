7ae069fce980b8a95d70f796b0e56d7eb4b3226f
Flaming Pasta <xnoeyes@naver.com>
2025-12-16 16:29:15 +0900

    Delete run/run_dpo_train.sh

--- run/run_dpo_train.sh
+++
@@ -1,6 +0,0 @@
-# run script for SFT training
-
-# export HF_HOME=~/.cache/huggingface
-# export TRANSFORMERS_CACHE=~/.cache/huggingface
-
-CUDA_VISIBLE_DEVICES=1,2 python -m scripts.train_dpo --path "output/2025-07-28_14-04-14_kakaocorp_kanana-1.5-8b-base_r_128_ra_128_rd_0.0_float16_sft"
